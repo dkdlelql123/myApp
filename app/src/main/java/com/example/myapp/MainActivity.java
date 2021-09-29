06bfb22f354ef505020abce8da9a09797a61e0ce
NamYeJi <dkdlelql123@naver.com>
2021-09-30 07:29:51 +0900

    계산기 빼기 구현

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public void btnAddClicked(View view) {
         EditText num1 = findViewById(R.id.activity_main__editTextNum1);
         EditText num2 = findViewById(R.id.activity_main__editTextNum2);
+        TextView textViewRs = findViewById(R.id.activity_main__textViewRs);
 
         num1.setText( num1.getText().toString().trim() );
         num2.setText( num2.getText().toString().trim() );
@@ -39,17 +41,47 @@ public class MainActivity extends AppCompatActivity {
         int rs = number1 + number2;
 
         toastMsg("결과 :"+rs);
+        textViewRs.setText("결과 :"+rs);
     }
 
     public void btnClearClicked(View view) {
         EditText editTextNum1 = findViewById(R.id.activity_main__editTextNum1);
         EditText editTextNum2 = findViewById(R.id.activity_main__editTextNum2);
+        TextView textViewRs = findViewById(R.id.activity_main__textViewRs);
 
         editTextNum1.setText("");
         editTextNum2.setText("");
         
         toastMsg("지우기완료");
+        textViewRs.setText("");
          
+    }
+
+    public void btnMinusClicked(View view) { EditText num1 = findViewById(R.id.activity_main__editTextNum1);
+        EditText num2 = findViewById(R.id.activity_main__editTextNum2);
+        TextView textViewRs = findViewById(R.id.activity_main__textViewRs);
+
+        num1.setText( num1.getText().toString().trim() );
+        num2.setText( num2.getText().toString().trim() );
+
+        if(num1.getText().toString().length()== 0){
+            toastMsg("숫자1을 작성해주세요.");
+            num1.requestFocus();
+            return;
+        }
+
+        if(num2.getText().toString().length()== 0){
+            toastMsg("숫자2을 작성해주세요.");
+            num2.requestFocus();
+            return;
+        }
+
+        int number1 = Integer.parseInt( num1.getText().toString() );
+        int number2 = Integer.parseInt( num2.getText().toString() );
+        int rs = number1 - number2;
+
+        toastMsg("결과 :"+rs);
+        textViewRs.setText("결과 :"+rs);
     }
     
     void toastMsg(String msg){
